e15ba077d39a36113ee70af8973661c4d0b80018
kodycook <masterkcook@hotmail.com>
2017-11-07 21:06:14 +1000

    Revomed auto-detect ip functionality and Web App config generator

--- src/main/java/com/cookware/home/server/MediaManager/Launcher.java
+++ src/main/java/com/cookware/home/server/MediaManager/Launcher.java
@@ -48,8 +48,6 @@ public class Launcher {
 
         instantiateDirectories();
 
-        createWebAppConfig(config);
-
         log.info("Launcher Started");
 
         MediaManager mediaManager = new MediaManager(config);
@@ -62,63 +60,11 @@ public class Launcher {
         webAppRequestHandler.start();
         mediaManager.start();
     }
+
     public static void instantiateDirectories(){
         DirectoryTools directoryTools = new DirectoryTools();
         directoryTools.createNewDirectory("logs");
         directoryTools.createNewDirectory("data");
         directoryTools.createNewDirectory("media");
-    }
-
-    public static void createWebAppConfig(Config config){
-        DirectoryTools directoryTools = new DirectoryTools();
-        directoryTools.createNewDirectory(config.webAppPath + "/resource");
-
-        String javascriptConfig = getJavascriptConfigAsString();
-
-        File file = new File(config.webAppPath + "/resource/config.js");
-        if(file.exists()){
-            file.delete();
-        }
-
-        try {
-            FileWriter fileWriter = new FileWriter(file);
-            fileWriter.write(javascriptConfig);
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            log.error("Couldn't create the JavaSript config");
-        }
-    }
-
-    public static String getJavascriptConfigAsString(){
-        Enumeration<NetworkInterface> n = null;
-        String result = "config = [";
-        int count = 0;
-        try {
-            n = NetworkInterface.getNetworkInterfaces();
-        } catch (SocketException e) {
-            log.error("Not Connected to network");
-        }
-        while (n.hasMoreElements())
-        {
-            NetworkInterface e = n.nextElement();
-
-            Enumeration<InetAddress> a = e.getInetAddresses();
-            for (; a.hasMoreElements();)
-            {
-                InetAddress addr = a.nextElement();
-
-                if (addr instanceof Inet6Address) continue;
-                count ++;
-                result += String.format("\n\t\"http://%s\",", addr.getHostAddress());
-            }
-        }
-        if(result.endsWith(","))
-        {
-            result = result.substring(0,result.length() - 1);
-        }
-        result += "\n]";
-
-        return result;
     }
 }
